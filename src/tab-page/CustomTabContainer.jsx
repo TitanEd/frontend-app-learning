f04d5c29373421d3fcc094fd1dd68a410a61d639
krishnah802 <kjulakanti@primesoft.net>
2025-08-07 10:22:04 +0530

    course-outline-page changes

--- src/tab-page/CustomTabContainer.jsx
+++ src/tab-page/CustomTabContainer.jsx
@@ -1,7 +1,4 @@
-import React, { Suspense, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
 import { OuterExamTimer } from '@edx/frontend-lib-special-exams';
 
 import { LmsBook } from '@openedx/paragon/icons';
@@ -11,37 +8,33 @@ import { useModel } from '../generic/model-store';
 const CustomTabContainer = (props) => {
   const {
     children,
-    fetch,
-    slice,
     tab,
-    isProgressTab,
+    courseId,
+    courseStatus,
+    metadataModel
   } = props;
 
-  const { courseId: courseIdFromUrl, targetUserId } = useParams();
-  const dispatch = useDispatch();
+  // const { courseId: courseIdFromUrl, targetUserId } = useParams();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    // The courseId from the URL is the course we WANT to load.
-    if (isProgressTab) {
-      dispatch(fetch(courseIdFromUrl, targetUserId));
-    } else {
-      dispatch(fetch(courseIdFromUrl));
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [courseIdFromUrl, targetUserId]);
+  // useEffect(() => {
+  //   // The courseId from the URL is the course we WANT to load.
+  //   if (isProgressTab) {
+  //     dispatch(fetch(courseIdFromUrl, targetUserId));
+  //   } else {
+  //     dispatch(fetch(courseIdFromUrl));
+  //   }
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [courseIdFromUrl, targetUserId]);
 
   // The courseId from the store is the course we HAVE loaded.  If the URL changes,
   // we don't want the application to adjust to it until it has actually loaded the new data.
-  const {
-    courseId,
-    courseStatus,
-  } = useSelector(state => state[slice]);
+  // const {
+  //   courseId,
+  //   courseStatus,
+  // } = useSelector(state => state[slice]);
 
   const {
-    courseAccess,
-    number,
-    org,
-    start,
     title,
   } = useModel('courseHomeMeta', courseId);
 
@@ -68,17 +61,15 @@ const CustomTabContainer = (props) => {
           <PluginSlot id="course_sidebar_plugin_slot" pluginProps={{ courseId, sidebarItems }} />
         </div> */}
         <main className="ca-main-content">
-          <Suspense>
             <TabPage
               activeTabSlug={tab}
               courseId={courseId}
               courseStatus={courseStatus}
-              metadataModel={`${slice}Meta`}
+              metadataModel={metadataModel}
             >
               {courseId && <OuterExamTimer courseId={courseId} />}
               {children}
             </TabPage>
-          </Suspense>
         </main>
       </div>
 
@@ -88,14 +79,7 @@ const CustomTabContainer = (props) => {
 
 CustomTabContainer.propTypes = {
   children: PropTypes.node.isRequired,
-  fetch: PropTypes.func.isRequired,
-  slice: PropTypes.string.isRequired,
   tab: PropTypes.string.isRequired,
-  isProgressTab: PropTypes.bool,
-};
-
-CustomTabContainer.defaultProps = {
-  isProgressTab: false,
 };
 
 export default CustomTabContainer;
--- src/tab-page/TabContainer.jsx
+++ src/tab-page/TabContainer.jsx
@@ -1,87 +1,11 @@
-// import React, { useEffect } from 'react';
-// import PropTypes from 'prop-types';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useParams } from 'react-router-dom';
-// import { OuterExamTimer } from '@edx/frontend-lib-special-exams';
-
-// import { PluginSlot } from '@openedx/frontend-plugin-framework';
-// import TabPage from './TabPage';
-
-// const TabContainer = (props) => {
-//   const {
-//     children,
-//     fetch,
-//     slice,
-//     tab,
-//     isProgressTab,
-//   } = props;
-
-//   const { courseId: courseIdFromUrl, targetUserId } = useParams();
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     // The courseId from the URL is the course we WANT to load.
-//     if (isProgressTab) {
-//       dispatch(fetch(courseIdFromUrl, targetUserId));
-//     } else {
-//       dispatch(fetch(courseIdFromUrl));
-//     }
-//   // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [courseIdFromUrl, targetUserId]);
-
-//   // The courseId from the store is the course we HAVE loaded.  If the URL changes,
-//   // we don't want the application to adjust to it until it has actually loaded the new data.
-//   const {
-//     courseId,
-//     courseStatus,
-//   } = useSelector(state => state[slice]);
-
-//   return (
-//     <PluginSlot
-//       id="custom_tab_container_plugin_slot"
-//       pluginProps={{
-//         tab,
-//         courseId,
-//         courseStatus,
-//         metadataModel,
-//       }}
-//     >
-//     <TabPage
-//       activeTabSlug={tab}
-//       courseId={courseId}
-//       courseStatus={courseStatus}
-//       metadataModel={`${slice}Meta`}
-//     >
-//       {courseId && <OuterExamTimer courseId={courseId} />}
-//       {children}
-//     </TabPage>
-//     </PluginSlot>
-//   );
-// };
-
-// TabContainer.propTypes = {
-//   children: PropTypes.node.isRequired,
-//   fetch: PropTypes.func.isRequired,
-//   slice: PropTypes.string.isRequired,
-//   tab: PropTypes.string.isRequired,
-//   isProgressTab: PropTypes.bool,
-// };
-
-// TabContainer.defaultProps = {
-//   isProgressTab: false,
-// };
-
-// export default TabContainer;
-
-import React, { Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { OuterExamTimer } from '@edx/frontend-lib-special-exams';
 
-import { LmsBook } from '@openedx/paragon/icons';
+import { PluginSlot } from '@openedx/frontend-plugin-framework';
 import TabPage from './TabPage';
-import { useModel } from '../generic/model-store';
 
 const TabContainer = (props) => {
   const {
@@ -112,52 +36,27 @@ const TabContainer = (props) => {
     courseStatus,
   } = useSelector(state => state[slice]);
 
-  const {
-    courseAccess,
-    number,
-    org,
-    start,
-    title,
-  } = useModel('courseHomeMeta', courseId);
-
   return (
-    <>
-      <div className="ca-breadcrumb-bg">
-        <div className="ca-breadcrumb-container">
-          <div className="ca-breadcrumb">
-            <span className="ca-breadcrumb-icon">
-              <LmsBook className="custom-icon" />
-              My Courses
-            </span>
-            <span className="ca-breadcrumb-divider">/</span>
-            <span className="ca-breadcrumb-current">{title || 'Loading...'}</span>
-          </div>
-          <div className="ca-title">
-            {title || 'Loading...'}
-          </div>
-        </div>
-      </div>
-      <div className="ca-main-layout">
-        {/* <MobileCourseNavigation items={sidebarItems} courseId={courseId} /> */}
-        {/* <div className="ca-sidebar">
-          <PluginSlot id="course_sidebar_plugin_slot" pluginProps={{ courseId, sidebarItems }} />
-        </div> */}
-        <main className="ca-main-content">
-          <Suspense>
-            <TabPage
-              activeTabSlug={tab}
-              courseId={courseId}
-              courseStatus={courseStatus}
-              metadataModel={`${slice}Meta`}
-            >
-              {courseId && <OuterExamTimer courseId={courseId} />}
-              {children}
-            </TabPage>
-          </Suspense>
-        </main>
-      </div>
-
-    </>
+    <PluginSlot
+      id="custom_tab_container_plugin_slot"
+      pluginProps={{
+        children,
+        tab,
+        courseId,
+        courseStatus,
+        metadataModel: `${slice}Meta`,
+      }}
+    >
+    <TabPage
+      activeTabSlug={tab}
+      courseId={courseId}
+      courseStatus={courseStatus}
+      metadataModel={`${slice}Meta`}
+    >
+      {courseId && <OuterExamTimer courseId={courseId} />}
+      {children}
+    </TabPage>
+    </PluginSlot>
   );
 };
 
